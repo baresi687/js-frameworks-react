85dd2f2e876e07427666b2a7c4182926bc856da2
hreinn <hreinngy@gmail.com>
2023-02-28 12:46:41 +0100

    Sass setup

---
+++ day-5-start-plain-css-sass/src/App.js
@@ -0,0 +1,17 @@
+import './sass/index.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Hello Hello Hello Hello Hello</p>
+      <ul>
+        <li>Dog</li>
+        <li>Dog</li>
+        <li>Dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
