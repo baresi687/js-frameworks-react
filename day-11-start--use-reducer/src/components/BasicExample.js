63dddb4d8bcdb8c7f48ba2575d1cb3df9ad7aced
hreinn <hreinngy@gmail.com>
2023-03-09 15:11:58 +0100

    useReducer

---
+++ day-11-start--use-reducer/src/components/BasicExample.js
@@ -0,0 +1,37 @@
+import {useReducer} from "react";
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return {count: state.count +1}
+    case ACTIONS.DECREMENT:
+      return {count: state.count -1}
+    default:
+      return state
+  }
+}
+const BasicExample = () => {
+  const [state, dispatch] = useReducer(reducer, {count: 20});
+
+  function increment() {
+    dispatch({type: ACTIONS.INCREMENT})
+  }
+
+  function decrement() {
+    dispatch({type: ACTIONS.DECREMENT})
+  }
+
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </>
+  );
+};
+
+export default BasicExample;
